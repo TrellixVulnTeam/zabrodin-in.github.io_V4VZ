6ccc15332fd159bdb1ad08da343e8b719dc7670f
Aleksandr <zabrodin.in@gmail.com>
2022-05-31 16:38:09 +0400

    JS module 5 // task 86 // Promise

---
+++ learning/2022-05 JScript & React/05 advanced JS/86 Promise/js/script.js
@@ -0,0 +1,70 @@
+'use strict';
+
+console.log('Data requested..');
+
+const req = new Promise(function(resolve, reject) {
+    setTimeout(() => {
+        console.log('Data prepairing..');
+        const product = {
+            name: 'TV',
+            price: 2000
+        };
+        resolve(product);
+    }, 2000);
+});
+
+
+// ////// FIRST VARIANT
+// req.then((product) => {
+//     const req2 = new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             product.status = 'order';
+//             resolve(product);
+//         }, 2000);
+//     });
+//     req2.then(data => {
+//         console.log(data);
+//     });
+// });
+
+
+////// SECOND VARIANT
+req.then((product) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            product.status = 'order';
+            resolve(product);
+            // reject();
+        }, 2000);
+    });
+}).then(data => {
+    data.modify = true;
+    return data;
+}).then(data => {
+    console.log(data);
+}).catch(() => {
+    console.error('Error occured');
+}).finally(() => {
+    console.log('finally');
+});
+
+
+///// TEST PROMISE
+const test = time => {
+    return new Promise(resolve => {
+        setTimeout(() => resolve(), time);
+    });
+};
+// test(2000).then(() => console.log('2000 ms'));
+// test(3000).then(() => console.log('3000 ms'));
+
+
+////// PROMISE ALL
+Promise.all([test(2000), test(4000)]).then(() => {
+    console.log('ALL');
+});
+
+////// PROMISE RACE
+Promise.all([test(1000), test(4000)]).then(() => { // .then will start after first func
+    console.log('ALL');
+});
