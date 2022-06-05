eefc63fc47ff8c7eb15eff1a347e08ae1f40de1e
Aleksandr <zabrodin.in@gmail.com>
2022-06-05 20:13:26 +0400

    JS module 5 // task 92-93 // creating sliders (2 options)

--- learning/2022-05 JScript & React/05 advanced JS/87, 85, 84, 79, 72, 69, 64 practice/js/script.js
+++ learning/2022-05 JScript & React/05 advanced JS/87, 85, 84, 79, 72, 69, 64 practice/js/script.js
@@ -384,4 +384,71 @@ window.addEventListener('DOMContentLoaded', () => {
     // fetch('http://localhost:3000/menu')
     //     .then(data => data.json())
     //     .then(res => console.log(res));
+
+/////////////////////////////////////////////
+//////////////// SLIDER VER.1 ///////////////
+    const slides = document.querySelectorAll('.offer__slide'),
+          btnPrev = document.querySelector('.offer__slider-prev'),
+          btnNext = document.querySelector('.offer__slider-next'),
+          countCurr = document.querySelector('#current'),
+          countTotal = document.querySelector('#total');
+    let slideIndex = 1;
+    // (slides.length < 10) ? countTotal.textContent = `0${slides.length}` : countTotal.textContent = `${slides.length}`;
+    
+    // showSlide(slideIndex);
+    // function showSlide(index) {
+    //     (index < 10) ? countCurr.textContent = `0${index}` : countCurr.textContent = `${index}`;
+    //     slides.forEach((elem, id) => {
+    //         if (id + 1 == index) {
+    //             elem.classList.add('show');
+    //             elem.classList.remove('hide');
+    //         } else {
+    //             elem.classList.add('hide');
+    //             elem.classList.remove('show');
+    //         }
+    //     });
+    // }
+    // btnPrev.addEventListener('click', () => {
+    //     slideIndex--;
+    //     if (slideIndex < 1) {
+    //         slideIndex = slides.length
+    //     }
+    //     showSlide(slideIndex);
+    // });
+    // btnNext.addEventListener('click', () => {
+    //     slideIndex++;
+    //     if (slideIndex > slides.length) {
+    //         slideIndex = 1;
+    //     }
+    //     showSlide(slideIndex);
+    // });
+/////////////////////////////////////////////
+//////////////// SLIDER VER.2 ///////////////
+    (slides.length < 10) ? countTotal.textContent = `0${slides.length}` : countTotal.textContent = `${slides.length}`;
+    const slidesWrapper = document.querySelector('.offer__slider-wrapper'),
+          slidesField = document.querySelector('.offer__slider-inner'),
+          width = window.getComputedStyle(slidesWrapper).width;
+    console.log(width);
+    slidesWrapper.style.width = width;
+    slidesField.style.display = 'flex';
+    slidesField.style.transition = '0.5s all';
+    slidesWrapper.style.overflow = 'hidden';
+    slidesField.style.width = 100 * slides.length + '%';
+    slides.forEach(item => {
+        item.style.width = width;
+    });
+    function changeCount(index) {
+        (index < 10) ? countCurr.textContent = `0${index}` : countCurr.textContent = `${index}`;
+    }
+    changeCount(slideIndex);
+    btnPrev.addEventListener('click', () => {
+        (slideIndex <= 1) ? slideIndex = slides.length : slideIndex--;
+        changeCount(slideIndex);
+        slidesField.style.transform = `translateX(-${(slideIndex - 1) * +width.slice(0, -2)}px)`;
+    });
+    btnNext.addEventListener('click', () => {
+        (slideIndex >= slides.length) ? slideIndex = 1 : slideIndex++;
+        changeCount(slideIndex);
+        slidesField.style.transform = `translateX(-${(slideIndex - 1) * +width.slice(0, -2)}px)`;
+    });
 });
