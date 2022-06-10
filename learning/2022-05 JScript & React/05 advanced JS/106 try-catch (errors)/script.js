d39b93c7f9dd5354c3657a2ea063fa9afc67ba2b
Aleksandr <zabrodin.in@gmail.com>
2022-06-10 12:06:39 +0400

    JS module 5 // task 106 // try/catch

---
+++ learning/2022-05 JScript & React/05 advanced JS/106 try-catch (errors)/script.js
@@ -0,0 +1,23 @@
+'use strict';
+
+try {
+    console.log('Hello try');
+    console.log(i) // error with var
+    console.log('Hello try second');
+} catch(error) {
+    console.log('\ncatch Hi!');
+    console.log(error);
+    console.dir(error.name);
+    console.dir(error.message);
+    console.dir(error.stack);
+} finally {
+    console.log('finally');
+}
+
+console.log('\nstill works');
+
+document.querySelector('.active').addEventListener('click', () => {
+    console.log('Click');
+});
+
+console.log('Normal');
