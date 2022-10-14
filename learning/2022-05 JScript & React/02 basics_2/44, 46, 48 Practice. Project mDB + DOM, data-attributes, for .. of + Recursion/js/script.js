aba392ce7420505feb613a30ac2a62d2c89d6d3f
Aleksandr <zabrodin.in@gmail.com>
2022-10-14 22:15:04 +0400

    JS module 2 // task 48 // Practice. Events

--- learning/2022-05 JScript & React/02 basics_2/44, 46, 48 Practice. Project mDB + DOM, data-attributes, for .. of + Recursion/js/script.js
+++ learning/2022-05 JScript & React/02 basics_2/44, 46, 48 Practice. Project mDB + DOM, data-attributes, for .. of + Recursion/js/script.js
@@ -12,9 +12,7 @@ const movieDB = {
     ]
 };
 
-
-/* Задания на урок:
-
+/* Задания на урок 44:
 1) Удалить все рекламные блоки со страницы (правая часть сайта)
 */
 const advBlock = document.querySelectorAll(`.promo__adv img`);
@@ -83,3 +81,54 @@ for (let node of document.body.childNodes) {
     }
     console.log(node);
 }
+
+/* Задания на урок 48:
+1) Реализовать функционал, что после заполнения формы и нажатия кнопки "Подтвердить" - 
+новый фильм добавляется в список. Страница не должна перезагружаться.
+Новый фильм должен добавляться в movieDB.movies.
+Для получения доступа к значению input - обращаемся к нему как input.value;
+P.S. Здесь есть несколько вариантов решения задачи, принимается любой, но рабочий.
+2) Если название фильма больше, чем 21 символ - обрезать его и добавить три точки
+3) При клике на мусорную корзину - элемент будет удаляться из списка (сложно)
+4) Если в форме стоит галочка "Сделать любимым" - в консоль вывести сообщение: 
+"Добавляем любимый фильм"
+5) Фильмы должны быть отсортированы по алфавиту */
+
+const form = document.querySelector(`form.add`),
+      formFilm = document.querySelector(`.adding__input`),
+      formButton = form.querySelector(`button`);
+
+function sortAndPrint(array) { // sorting the list & printing new data
+    array.sort();
+    let ul = document.querySelector(`.promo__interactive-list`);
+    ul.innerHTML = ``;
+    array.forEach((value, index) => {
+        let li = document.createElement('li');
+        li.classList.add(`promo__interactive-item`);
+        if (value.length > 21) {
+            li.innerHTML += `${index + 1}) ` + value.slice(0, 21) + `...`;
+        } else {
+            li.innerHTML += `${index + 1}) ` + value;
+        }
+        li.innerHTML += `<div class="delete"></div>`;
+        ul.append(li);
+    });
+    
+    document.querySelectorAll(`.delete`).forEach((value, index) => { // deleting films
+        value.addEventListener('click', () => {
+            console.log(`${array[index]}`);
+            delete array[index];
+            sortAndPrint(array);
+        });
+    });
+}
+
+// adding a new films
+formButton.addEventListener('click', function(event) {
+    event.preventDefault();
+    movieDB.movies.push(formFilm.value);
+    sortAndPrint(movieDB.movies);
+    if (form.querySelector(`[data-checkbox="check"]`).checked) {
+        console.log(`Added new favourite film`);
+    }
+});
