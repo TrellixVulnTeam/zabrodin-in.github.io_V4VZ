5ba72f70dab7e7e86ec80b9a62ea1818021e2f4f
Aleksandr <zabrodin.in@gmail.com>
2022-11-16 11:37:09 +0400

    JS module 5 // task 115 // Macro & micro tasks

---
+++ learning/2022-05 JScript & React/05 advanced JS_2/115 macro:micro/script.js
@@ -0,0 +1,33 @@
+'use strict';
+
+console.log('1');
+setTimeout(() => {console.log('2');}); // with NO timeout
+Promise.resolve()
+    .then(() => console.log('3'));
+    
+queueMicrotask(() => console.log('3.5')); //////
+// console.log('3.5');
+
+Promise.resolve()
+    .then(() => console.log('4'));
+console.log('5');
+
+// () => {} ----- macro task
+// microtasks await/catch/then/finally
+// render
+// () => {} ----- macro task
+// microtasks await/catch/then/finally
+// render
+// <..>
+'use strict';
+ 
+function getSum(a, b) {
+    function sum() {
+        console.log(this.a);
+        return a + b;
+    }
+ 
+    console.log(sum());
+}
+ 
+getSum(4, 5);
