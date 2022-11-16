fa01047c59ae440de4b4db72533d78d8005166be
Aleksandr <zabrodin.in@gmail.com>
2022-11-16 23:14:33 +0400

    JS module 6 // task 122 // JSX

---
+++ learning/2022-05 JScript & React/06 React base/121, 122 create React app + JSX/my-app/src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+
+const text = `Hello World!`;
+const elem = (
+  <div>
+    <h2 className='name'>{text}</h2>
+    <input type="text" />
+    <label htmlFor=""></label>
+    <button tabIndex={0}>Click</button>
+    
+  </div>
+  );
+
+// const elem = React.createElement('h2', {className: 'greetings'}, 'Hello World!');
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  elem
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>
+);
