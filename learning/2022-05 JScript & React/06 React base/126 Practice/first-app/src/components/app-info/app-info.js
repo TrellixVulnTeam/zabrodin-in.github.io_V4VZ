22dd8616f599735aa2e95631ba15bafebb2f16d4
Aleksandr <zabrodin.in@gmail.com>
2022-11-17 17:12:37 +0400

    JS module 6 // task 126 // Practice (Employees)

---
+++ learning/2022-05 JScript & React/06 React base/126 Practice/first-app/src/components/app-info/app-info.js
@@ -0,0 +1,13 @@
+import './app-info.css';
+
+const AppInfo = () => {
+    return (
+        <div className="app-info">
+            <h1>Accounting of employees in the company N</h1>
+            <h2>Total numbers of employees: </h2>
+            <h2>the following will receive the award: </h2>
+        </div>
+    )
+}
+
+export default AppInfo;
---
+++ learning/2022-05 JScript & React/06 React base/126 Practice/first-app/src/components/employees-list-item/employees-list-item.js
@@ -0,0 +1,25 @@
+import './employees-list-item.css';
+
+const EmployeesListItem = () => {
+    return (
+        <li className="list-group-item d-flex justify-content-between">
+            <span className='list-group-item-label'>John Smith</span>
+            <input type="text" defaultValue="$1000" className="list-group-item-input" />
+            <div className="d-flex justify-content-center align-items-center">
+                <button 
+                    className="btn-cookie btn-sm"
+                    type="button">
+                        <i className="fas fa-cookie"></i>
+                </button>
+                <button 
+                    className="btn-trash btn-sm"
+                    type="button">
+                        <i className="fas fa-trash"></i>
+                </button>
+                <i className="fas fa-star"></i>
+            </div>
+        </li>
+    )
+}
+
+export default EmployeesListItem;
