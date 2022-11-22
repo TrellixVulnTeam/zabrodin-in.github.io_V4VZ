2bd4ebafc735461ba68731cf8a463c06b45d0264
Aleksandr <zabrodin.in@gmail.com>
2022-11-22 12:21:30 +0400

    JS module 6 // task 134 // Practice (Employee) add form events

---
+++ learning/2022-05 JScript & React/06 React base/126, 127, 128, 129, 133, 134 Practice (Employees)/first-app/src/components/employees-add-form/employees-add-form.js
@@ -0,0 +1,49 @@
+import {Component} from 'react';
+import './employees-add-form.css';
+
+class EmployeesAddForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            salary: ''
+        }
+    }
+
+    onValueChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    render() {
+        const {name, salary} = this.state;
+        return (
+            <div className="app-add-form">
+                <h3>Add new employee</h3>
+                <form 
+                    action="" 
+                    className="add-form d-flex">
+                        <input 
+                            type="text" 
+                            className="form-control new-post-label" 
+                            placeholder="What's his name?" 
+                            onChange={this.onValueChange} 
+                            name="name" 
+                            value={name} />
+                        <input 
+                            type="number" 
+                            className="form-control new-post-label" 
+                            placeholder="$$ Salary" 
+                            onChange={this.onValueChange} 
+                            name="salary" 
+                            value={salary} />
+                        <button className="btn btn-outline-light">
+                            Add</button>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default EmployeesAddForm;
